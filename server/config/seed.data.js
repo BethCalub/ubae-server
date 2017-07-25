d0662c14be951baeda4c8353101db3a276ad8064
earmix <earmix@outlook.com>
2017-07-25 17:59:31 +0800

    seed

--- server/config/seed.data.js
+++ server/config/seed.data.js
@@ -739,8 +739,183 @@ let sihtm = {
 let slahs = {
   department: 'SCHOOL OF LIBERAL ARTS AND HUMAN SCIENCES',
   mission: 'The University of Baguio educates individuals to be empowered professionals in the global community. The School of Liberal Arts and Human Sciences provides its students with total education apt to their chosen career.',
-  tags: ['SLAHS']
+  objectives: {
+    header: 'The School of Liberal Arts and Human Science aims to produce a graduate who:',
+    list: ['1. demonstrates pertinent and cooperative competence befitting a wholly developed individual;',
+      '2. adheres to a higher standard of learning founded under the tenets of systematic knowledge, analytical thinking, oral and written communication, arts and leadership;',
+      '3. generates an epitome of a socially and professionally accepted code of conduct;',
+      '4. conducts researches in the promotion of change and social progress;',
+      '5. nurtures an environment fit for a healthier mind and body; and',
+      '6. possesses the proper outlook and initiative to pursue related fields of learning.'
+    ]
+  },
+  location: {
+    bldg: '',
+    floor: '',
+    room: ''
+  },
+  tags: ['SLAHS', 'Liberal'],
+  programs: [{
+    name: 'Master in Public Administration',
+    requirements: '',
+    prereq: '',
+    years: null,
+    programType: '',
+    objectives: {
+      header: 'The Master in Public Administration Program aims to produce a graduate who:',
+      list: ['1. demonstrates well-rounded and integrative knowledge in public management and governance;',
+        '2. performs responsible administrative and managerial functions in public agencies;',
+        '3. embodies the ideals and ethical standards of a public administrator;',
+        '4. conducts pertinent researches for the enhancement of public services;',
+        '5. imbibes positive influence to address local and national public management issues; and',
+        '6. exemplifies a strong sense of dynamic Filipinism and responsible citizenship.'
+      ]
+    },
+    tags: ['Public Administration']
+  }, {
+    name: 'Master of Arts in English',
+    requirements: '',
+    prereq: '',
+    years: null,
+    programType: 'Graduate',
+    objectives: {
+      header: 'The Master of Arts in English Program aims to produce a graduate who:',
+      list: ['1. displays exceptional competencies in the fields of language and literature;',
+        '2. exudes a high level of skills in analyzing and interpreting different genres;',
+        '3. acts as a role model of professionalism in addressing various societal concerns;',
+        '4. undertakes and utilizes language and literature-related researches for community development;',
+        '5. promotes positive ideas of the literary works in improving society; and',
+        '6. demonstrates language proficiency in the exercise of the profession.'
+      ]
+    },
+    tags: ['Master in English']
+  }, {
+    name: 'Bachelor of Arts in Broadcasting',
+    requirements: '',
+    prereq: '',
+    years: 4,
+    programType: 'Baccalaureate',
+    objectives: {
+      header: 'The Bachelor of Arts in Communication and Broadcasting Program aims to produce a graduate who: ',
+      list: ['1. actualizes the role of media in the transformation of the society;',
+        '2. exudes competence in media communication;',
+        '3. sustains the highest ethical standards performed by a media practitioner;',
+        '4. conducts researches in mass communication that promote change and development;',
+        '5. participates actively in development communication programs; and',
+        '6. manifests and maintains commendable performance in the media practice.'
+      ]
+    },
+    tags: ['Broadcasting']
+  }, {
+    name: 'Bachelor of Arts in Mass Communication',
+    requirements: '',
+    prereq: '',
+    years: 4,
+    programType: 'Baccalaureate',
+    objectives: {
+      header: 'The Bachelor of Arts in Communication and Broadcasting Program aims to produce a graduate who: ',
+      list: ['1. actualizes the role of media in the transformation of the society;',
+        '2. exudes competence in media communication;',
+        '3. sustains the highest ethical standards performed by a media practitioner;',
+        '4. conducts researches in mass communication that promote change and development;',
+        '5. participates actively in development communication programs; and',
+        '6. manifests and maintains commendable performance in the media practice.'
+      ]
+    },
+    tags: ['Masscom', 'Mass Communication']
+  }, {
+    name: 'Bachelor of Arts in English',
+    requirements: '',
+    prereq: '',
+    years: 4,
+    programType: 'Baccalaureate',
+    objectives: {
+      header: 'The Bachelor of Arts in English Program aims to produce a graduate who: ',
+      list: ['1. exhibits a competitive quality of education in the fields of language and literature;',
+        '2. displays written and oral proficiency in using the English language;',
+        '3. promotes better understanding among people through literature;',
+        '4. contributes to the preservation and promotion of the nation and its culture through writings and researches;',
+        '5. participates actively in the promotion of community and environment related activities through arts and humanities; and',
+        '6. manifests notable performance as an English language practitioner'
+      ]
+    },
+    tags: ['Bachelor English']
+  }, {
+    name: 'Bachelor of Arts in Political Science',
+    requirements: '',
+    prereq: '',
+    years: 4,
+    programType: 'Baccalaureate',
+    objectives: {
+      header: 'The Bachelor of Arts in Political Science Program aims to produce a graduate who: ',
+      list: ['1. exhibits integrative knowledge and skills in managing political institutions and processes;',
+        '2. assumes leadership positions in public service',
+        '3. instills to others good and responsible citizenship;',
+        '4. conducts research that contributes to the preservation and promotion of the nation and its political undertakings;',
+        '5. sustains the effective expression and dissemination of ideas in community development activities; and',
+        '6. leads in the promotion of good governance.'
+      ]
+    },
+    tags: ['Polsci', 'Political Science']
+  }, {
+    name: 'Bachelor of Science in Psychology',
+    requirements: '',
+    prereq: '',
+    years: 4,
+    programType: 'Baccalaureate',
+    objectives: {
+      header: 'The Bachelor of Science in Psychology Program aims to produce a graduate who: ',
+      list: ['1. manifests a comprehensive knowledge in human behavior and mental processes;',
+        '2. exhibits competence in the practice of the profession;',
+        '3. practices ethical standards by upholding strict confidentiality of records;',
+        '4. generates significant behavioral researches for theoretical advancement and practical applications;',
+        '5. promotes involvement in programs that cater to community welfare and development; and',
+        '6. pursues excellence through unique and creative practices in the application of principles and theories of psychology.'
+      ]
+    },
+    tags: ['Psych', 'Pscyhology']
+  }, {
+    name: 'Bachelor of Arts in Music',
+    requirements: '',
+    prereq: '',
+    years: 4,
+    programType: 'Baccalaureate',
+    objectives: {
+      header: 'The Bachelor of Arts in Music Program aims to produce a graduate who: ',
+      list: ['1. manifests specialized skills and knowledge for musical instruments, composition, and choral conducting;',
+        '2. exhibits a high standard of technical ability and artistry in music;',
+        '3. achieves an acceptable level of depth and breadth of repertoire;',
+        '4. generates researches focused on Philippine music particularly Cordilleran indigenous music;',
+        '5. promotes non-controversial music in community-related activities; and',
+        '6. pursues a successful career in the field of music, both as a performer and a teacher.'
+      ]
+    },
+    tags: ['Bachelor Music']
+  }, {
+    name: 'Extension classes in Music',
+    requirements: '',
+    prereq: '',
+    years: null,
+    programType: 'Special',
+    objectives: {
+      header: '',
+      list: ['']
+    },
+    tags: ['Music Class']
+  }, {
+    name: 'Short courses in Photography and Digital Arts',
+    requirements: '',
+    prereq: '',
+    years: null,
+    programType: 'Special',
+    objectives: {
+      header: '',
+      list: ['']
+    },
+    tags: ['Photography', 'Digital Arts']
+  }]
 };
+
 
 exports.departments = [sbaa, scjps, sod, sea, sit, sihtm, sol, slahs, sns, son, ste];
 
