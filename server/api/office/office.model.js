365323fd145250aef26f4152edfc31cb5276d64f
earmix <earmix@outlook.com>
2017-07-27 13:21:53 +0800

    office model

--- server/api/office/office.model.js
+++ server/api/office/office.model.js
@@ -4,8 +4,21 @@ import mongoose from 'mongoose';
 import {registerEvents} from './office.events';
 
 var OfficeSchema = new mongoose.Schema({
-  name: String,
-  info: String,
+  office: String,
+  description: String,
+  location: {
+    bldg: String,
+    floor: String,
+    room: String,
+  },
+  tags: [String],
+  services: [{
+    name: String,
+    description: String,
+    requirements: [String],
+    process: [String],
+    tags: [String]
+  }],
   active: Boolean
 });
 
