40408a894a1fc124574638a89092b1e8fdaa14d2
earmix <earmix@outlook.com>
2017-07-27 12:42:26 +0800

    seed clean up

--- server/config/seed.data.js
+++ server/config/seed.data.js
@@ -1,14 +1,31 @@
 'use strict';
-const sbaa = require('./data/dept.sbaa.js');
-const sit = require('./data/dept.sit.js');
-const slahs = require('./data/dept.slahs.js');
-const sod = require('./data/dept.sod.js');
-const sol = require('./data/dept.sol.js');
-const ste = require('./data/dept.ste.js');
-const sea = require('./data/dept.sea.js');
-const scjps = require('./data/dept.scjps.js');
-const son = require('./data/dept.son.js');
-const sns = require('./data/dept.sns.js');
-const sihtm = require('./data/dept.sihtm.js');
+//Departments
+const sbaa = require('./data/dept/dept.sbaa.js');
+const sit = require('./data/dept/dept.sit.js');
+const slahs = require('./data/dept/dept.slahs.js');
+const sod = require('./data/dept/dept.sod.js');
+const sol = require('./data/dept/dept.sol.js');
+const ste = require('./data/dept/dept.ste.js');
+const sea = require('./data/dept/dept.sea.js');
+const scjps = require('./data/dept/dept.scjps.js');
+const son = require('./data/dept/dept.son.js');
+const sns = require('./data/dept/dept.sns.js');
+const sihtm = require('./data/dept/dept.sihtm.js');
 
-exports.departments = [sbaa.sbaa, scjps.scjps, sod.sod, sea.sea, sit.sit, sihtm.sihtm, sol.sol, slahs.slahs, sns.sns, son.son, ste.ste];
+//Offices
+
+exports.departments = [
+  sbaa.sbaa,
+  scjps.scjps,
+  sod.sod,
+  sea.sea,
+  sit.sit,
+  sihtm.sihtm,
+  sol.sol,
+  slahs.slahs,
+  sns.sns,
+  son.son,
+  ste.ste
+];
+
+exports.offices = [];
