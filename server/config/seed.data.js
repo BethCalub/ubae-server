f8ed73451ae799e923ebe95f470e49e410d392fc
earmix <earmix@outlook.com>
2017-07-27 12:51:27 +0800

    seed update cleanup

--- server/config/seed.data.js
+++ server/config/seed.data.js
@@ -1,29 +1,28 @@
 'use strict';
 //Departments
 const sbaa = require('./data/dept/dept.sbaa.js');
+const scjps = require('./data/dept/dept.scjps.js');
+const sea = require('./data/dept/dept.sea.js');
+const sihtm = require('./data/dept/dept.sihtm.js');
 const sit = require('./data/dept/dept.sit.js');
 const slahs = require('./data/dept/dept.slahs.js');
+const sns = require('./data/dept/dept.sns.js');
 const sod = require('./data/dept/dept.sod.js');
 const sol = require('./data/dept/dept.sol.js');
-const ste = require('./data/dept/dept.ste.js');
-const sea = require('./data/dept/dept.sea.js');
-const scjps = require('./data/dept/dept.scjps.js');
 const son = require('./data/dept/dept.son.js');
-const sns = require('./data/dept/dept.sns.js');
-const sihtm = require('./data/dept/dept.sihtm.js');
+const ste = require('./data/dept/dept.ste.js');
 
 //Offices
-
 exports.departments = [
   sbaa.sbaa,
   scjps.scjps,
-  sod.sod,
   sea.sea,
-  sit.sit,
   sihtm.sihtm,
-  sol.sol,
+  sit.sit,
   slahs.slahs,
   sns.sns,
+  sod.sod,
+  sol.sol,
   son.son,
   ste.ste
 ];
