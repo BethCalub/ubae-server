e9759ccbe974a3f239c64f3d6f26356948494798
earmix <earmix@outlook.com>
2017-10-18 21:37:12 +0800

    update admin dashboard

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -4,33 +4,125 @@ import routing from './main.routes';
 
 export class MainController {
   /*@ngInject*/
-  constructor($http, $scope, socket, $window) {
+  constructor($http, $scope, socket, $window, $uibModal, $log, $document) {
     this.$http = $http;
     this.socket = socket;
+    this.$uibModal = $uibModal;
+    this.$document = $document;
+    this.$log = $log;
 
-    this.totalItems = 64;
-    this.currentPage = 4;
+    this.startDate = '';
+    this.altInputFormats = ['M!/d!/yyyy'];
+    this.startDatePicker = {
+      isDatepickerOpen: false,
+      datepickerOptions: {
+        datepickerMode: 'day',
+        showWeeks: true,
+        startingDay: 0, // (0=Sunday, ..., 6=Saturday)
+        minDate: new Date(Date.now()), // must be JS Date
+        maxDate: null, // must be JS Date
+        initDate: null // must be JS Date
+      }
+    };
+    this.endDate = '';
+    this.endDatePicker = {
+      isDatepickerOpen: false,
+      datepickerOptions: {
+        datepickerMode: 'day',
+        showWeeks: true,
+        startingDay: 0, // (0=Sunday, ..., 6=Saturday)
+        minDate: new Date(Date.now()), // must be JS Date
+        maxDate: null, // must be JS Date
+        initDate: null // must be JS Date
+      }
+    };
 
-    this.isCollapsed = true;
-
-    this.tabs = [
-      { title: 'Dynamic Title 1', content: 'Dynamic content 1' },
-      { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }
-    ];
+    this.locations = '/api/locations';
+    this.events = '/api/events';
+    this.informations = '/api/informations';
+    this.providers = '/api/providers';
+    this.instructions = '/api/instructions';
+    this.responses = '/api/responses';
+    this.feedbacks = '/api/feedbacks';
+    this.items = ['item1', 'item2', 'item3'];
 
     this.alertMe = function(message) {
       setTimeout(function() {
         $window.alert(message);
       });
     };
-  
-    this.model = {
-      name: 'Tabs'
-    };
   }
 
   $onInit() {
+    this.$http.get(this.locations)
+    .then(response => {
+      this.locationEntries = response.data;
+      this.eventStatus = 'Locations Loaded';
+      console.log(response.statusText);
+    }, err => {
+      this.eventStatus = 'Cannot Load Data';
+      console.log(err.statusText);
+    });
 
+    this.$http.get(this.events)
+    .then(response => {
+      this.eventEntries = response.data;
+      this.eventStatus = 'Locations Loaded';
+      console.log(response.statusText);
+    }, err => {
+      this.eventStatus = 'Cannot Load Data';
+      console.log(err.statusText);
+    });
+
+    this.$http.get(this.informations)
+    .then(response => {
+      this.informationEntries = response.data;
+      this.eventStatus = 'Locations Loaded';
+      console.log(response.statusText);
+    }, err => {
+      this.eventStatus = 'Cannot Load Data';
+      console.log(err.statusText);
+    });
+
+    this.$http.get(this.providers)
+    .then(response => {
+      this.providerEntries = response.data;
+      this.eventStatus = 'Locations Loaded';
+      console.log(response.statusText);
+    }, err => {
+      this.eventStatus = 'Cannot Load Data';
+      console.log(err.statusText);
+    });
+
+    this.$http.get(this.instructions)
+    .then(response => {
+      this.instructionEntries = response.data;
+      this.eventStatus = 'Locations Loaded';
+      console.log(response.statusText);
+    }, err => {
+      this.eventStatus = 'Cannot Load Data';
+      console.log(err.statusText);
+    });
+
+    this.$http.get(this.responses)
+    .then(response => {
+      this.responseEntries = response.data;
+      this.eventStatus = 'Locations Loaded';
+      console.log(response.statusText);
+    }, err => {
+      this.eventStatus = 'Cannot Load Data';
+      console.log(err.statusText);
+    });
+
+    this.$http.get(this.feedbacks)
+    .then(response => {
+      this.feedbackEntries = response.data;
+      this.eventStatus = 'Locations Loaded';
+      console.log(response.statusText);
+    }, err => {
+      this.eventStatus = 'Cannot Load Data';
+      console.log(err.statusText);
+    });
   }
 }
 
